d7aee6dfb67be0a150945280184a2f44a6fdb82e
Matthew Ford <matt@bitzesty.com>
2016-03-27 02:31:49 +0100

    add in a executable

--- lib/harvest_slack_report.rb
+++ lib/harvest_slack_report.rb
@@ -1,5 +1,10 @@
-require "harvest_slack_report/version"
+require 'harvest_slack_report/version'
 
+# Posts summary harvest data to a slack channel
 module HarvestSlackReport
-  # Your code goes here...
+  def self.run
+    puts 'Collecting Harvest data'
+    puts 'Aggregating data'
+    puts 'Posting to Slack'
+  end
 end
