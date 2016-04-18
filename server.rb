a2c948729b456010e4780337557c30eacf71db46
Matthew Ford <matt@bitzesty.com>
2016-04-18 20:36:59 +0100

    basic auth

--- server.rb
+++ server.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 
 class HarvestApp < Sinatra::Base
+  use Rack::Auth::Basic, 'Nope' do |username, password|
+    username == ENV.fetch('AUTH_USER') && password == ENV.fetch('AUTH_PASSWORD')
+  end
+
   get '/' do
     'OK'
   end
